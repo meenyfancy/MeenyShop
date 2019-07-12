f50b0effdc354e16940b4431e83c45225a389121
nattakornp <nattakornp@gmail.com>
2019-07-13 02:37:42 +0700

    no message

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-$channel_token ='qFZ908h/98NwePyPmA1J0FFmgWvYZjXYmCozPAmqENRhKjBG4HLxwIZhztKCyLWOcA1bzhaKTdpRtYvDCidqWSqJaNl/9N8POwRxKjGMmWLT5Zmk1mYIOtUU+C2Rdna0CE4r1zI4SyZPNuED8nx7mwdB04t89/1O/w1cDnyilFU=';
-$channel_secret = 'c14961f4925ae68f04875b8460fb0f5f';
+$channel_token ='qdfDhN/jk/JSII9PqmifBCgoJSfx7x6SI4Zu6Jv6BeNJq1uYdTkcdn5ZtzfE0lMesaEk98zP6nT2nubKWdvs7BOKmN1a+kGjnlde8Wnl5MLlp6X2hFfWHtkLGba/UhWP0gdoOENp245dAV2w5jX8SAdB04t89/1O/w1cDnyilFU=';
+$channel_secret = '80074d0a07f171aab6a73fd7f21997ae';
 // Get message from Line API
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
